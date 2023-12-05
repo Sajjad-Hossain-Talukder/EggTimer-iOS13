e74a468b03e9ecd1d3eb73dd9cf87fdb2c58a566
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-05 16:20:43 +0600

    timer added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,49 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft" : 3 , "Medium" : 4 , "Hard" : 7 ]
+    let eggTimes = ["Soft" : 3.0 , "Medium" : 4.0 , "Hard" : 7.0 ]
     
-    var secondsRemaining : Int = 0
-    var titleSecondsRemaining : Int = 0
+    var secondsRemaining : Float = 0
+    var titleSecondsRemaining : Float = 0
+    var total : Float = 0.0
+    var passed : Float = 0.0
     var timer = Timer()
     
-    @IBOutlet weak var actionButton: UILabel!
+
+    @IBOutlet weak var titleAction: UILabel!
+    
+    @IBOutlet weak var progressTr: UIProgressView!
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
+        progressTr.progress = 0.0
         let button = sender.currentTitle!
-        secondsRemaining = eggTimes[button]!
-        
+        secondsRemaining = Float(eggTimes[button]!)
+        passed = 0
+        total =  Float(eggTimes[button]!)
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(updateTimer), userInfo: nil, repeats: true )
-        
-        
         
     }
     
     
     @objc func updateTimer (){
-        if self.secondsRemaining > 0 {
-            print ("\(self.secondsRemaining) seconds")
+        if self.secondsRemaining > 0.0 {
+            //print ("\(self.secondsRemaining) seconds")
             self.secondsRemaining -= 1
+            self.passed += 1.0
+            var percnt = ( self.passed / self.total )
+            //print(percnt)
+            progressTr.progress = percnt
+            
         } else {
             timer.invalidate()
-            actionButton.text = "Done!!!"
-            titleSecondsRemaining = 10
-            timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector:#selector(titleTimer), userInfo: nil, repeats: true )
+            titleAction.text = "Done!!!"
+            titleSecondsRemaining = 20.0
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(titleTimer), userInfo: nil, repeats: true )
         }
     }
     
     @objc func titleTimer (){
         
-        if self.titleSecondsRemaining > 0 {
+        if self.titleSecondsRemaining > 0.0 {
             self.titleSecondsRemaining -= 1
         } else {
             timer.invalidate()
-            actionButton.text = "How do you like your eggs?"
+            progressTr.progress = 0.0
+            titleAction.text = "How do you like your eggs?"
         }
         
-    
     }
     
     
